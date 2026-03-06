9931ad63d275228767b0ec015ca205d3a9b690c4
Amruth U <avcareer107@gmail.com>
2026-03-06 23:38:10 +0530

    Day8: practised one more recursion

--- JavaProgram7.java
+++ JavaProgram7.java
@@ -1,8 +1,10 @@
 public class JavaProgram7{
     public static void main(String[] args){
-        int n  = 10;
+        int n  = 5;
         int result = Get_Sum(n);
         System.out.println(result);
+        int factorial = Factorial(n);
+        System.out.println(factorial);
     }
     public static int Get_Sum(int n){
         if(n==0){
@@ -11,5 +13,13 @@ public class JavaProgram7{
 
         return n+Get_Sum(n-1);
     }
-    
+    public static int Factorial(int n){
+        if (n<0){
+            System.out.println("cannot get factorial of negative no.s");
+        }
+        else if(n==0){
+            return 1;
+        }
+        return n*Factorial(n-1);
+    }
 }
