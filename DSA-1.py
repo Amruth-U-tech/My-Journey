4c2d191cfb6329204d1dbe51c478801eb130d4cd
Amruth U <avcareer107@gmail.com>
2026-03-12 22:42:08 +0530

    DSA-3 solved (arrays)

--- DSA-1.py
+++ DSA-1.py
@@ -11,6 +11,18 @@ class Solution(object):
             l.append(nums[i])
             l.append(nums[i+n])
         return l
+    def findMaxConsecutiveOnes(self, nums):
+        max1 = 0
+        iterations = 0
+        for i in nums:
+            if i==1:
+                iterations+=1
+            else:
+                if max1<iterations:
+                    max1=iterations 
+                iterations=0
+        return(max([iterations,max1]))
+        
 sol = Solution()
 nums = [1,3,2,1]
 sol.getConcatenation(nums)
