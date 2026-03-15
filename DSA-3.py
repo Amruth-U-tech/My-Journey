7cd11a5e41ce3095e2a668997cc89644075d6a31
Amruth U <avcareer107@gmail.com>
2026-03-15 19:26:09 +0530

    DSA-3:added new optimal code

--- DSA-3.py
+++ DSA-3.py
@@ -10,3 +10,26 @@ class Solution(object):
                 return i
             left_sum += nums[i]
         return -1
+    
+    def findDisappearedNumbers(self, nums):
+        hashset = set()
+        missing = []
+
+        for i in nums:
+            if i not in hashset:
+                hashset.add(i)
+        
+        for i in range(1,len(nums)+1):
+            if i not in hashset:
+                missing.append(i)
+        return missing
+    def findDisappearedNumbers(self, nums):
+        missing = []
+        for i in range(len(nums)):
+            index = abs(nums[i])-1
+            nums[index] = -abs(nums[index])
+
+        for i in range(len(nums)):
+            if nums[i]>0:
+                missing.append(i+1) 
+        return missing
