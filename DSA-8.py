1c468ee811a801ffa1d9a3697b15f72320e020d1
Amruth U <avcareer107@gmail.com>
2026-03-23 19:56:52 +0530

    DSA-8: solved one more prefix sum+hashmaps

--- DSA-8.py
+++ DSA-8.py
@@ -11,3 +11,23 @@ class Solution(object):
                 count+=hashing[prefix_sum-k]
             hashing[prefix_sum]=hashing.get(prefix_sum,0)+1
         return count
+    def findMaxLength(self, nums):
+        prefix_sum = 0
+        hashmap = {0: -1}
+        max_len = 0
+
+        for i in range(len(nums)):
+
+            if nums[i] == 0:
+                prefix_sum -= 1
+            else:
+                prefix_sum += 1
+
+            if prefix_sum in hashmap:
+                length = i - hashmap[prefix_sum]
+                max_len = max(max_len, length)
+            else:
+                hashmap[prefix_sum] = i
+
+        return max_len
+        
