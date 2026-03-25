ca1e351c45cfaeedafe56a6a8a5673ffd0cc111b
Amruth U <avcareer107@gmail.com>
2026-03-25 23:44:30 +0530

    DSA-9: solved stack and greedy problem

--- DSA-9.py
+++ DSA-9.py
@@ -19,3 +19,22 @@ class Solution(object):
             max_length = max(max_length, right - left + 1)
 
         return max_length
+
+    def removeKdigits(self, num, k):
+        stack = []
+
+        for digit in num:
+            while stack and k > 0 and stack[-1] > digit:
+                stack.pop()
+                k -= 1
+            stack.append(digit)
+
+        # If k still remains, remove from end
+        while k > 0:
+            stack.pop()
+            k -= 1
+
+        # Remove leading zeros
+        result = ''.join(stack).lstrip('0')
+
+        return result if result else "0"
