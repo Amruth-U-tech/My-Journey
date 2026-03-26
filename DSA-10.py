3b3bc84685e7e487e204c20b6b03615f33bdeeb6
Amruth U <avcareer107@gmail.com>
2026-03-26 22:12:17 +0530

    DSA-10: revision of prefix and sufix sum

--- DSA-10.py
+++ DSA-10.py
@@ -8,3 +8,19 @@ class Solution(object):
             max_sum = max(max_sum, current_sum)
 
         return max_sum
+    
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        res = [1]*n
+
+        prefix = 1
+        for i in range(n):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        suffix = 1
+        for i in range(n-1, -1, -1):
+            res[i] *= suffix
+            suffix *= nums[i]
+
+        return res
