d192efda8371a78df93c3fbd045df0b2dbc14cd6
Amruth U <avcareer107@gmail.com>
2026-03-28 17:09:22 +0530

    DSA-11

--- DSA-11.py
+++ DSA-11.py
@@ -1,26 +1,4 @@
 tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
-
-# stack = []
-
-# for token in tokens:
-#     if token in {"+", "-", "*", "/"}:
-#         b = stack.pop()
-#         a = stack.pop()
-
-#         if token == "+":
-#             stack.append(a + b)
-#         elif token == "-":
-#             stack.append(a - b)
-#         elif token == "*":
-#             stack.append(a * b)
-#         elif token == "/":
-#             stack.append(int(a / b))
-
-#     else:
-#         stack.append(int(token))
-
-# print(stack)
-
 class Solution(object):
     def evalRPN(self, tokens):
         stack = []
@@ -58,6 +36,4 @@ class Solution(object):
                 continue
             l.append("Push")
             l.append("Pop")
-            
-
         return l
