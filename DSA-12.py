eba738896b2f91e2a432814764986d57c6f692ff
Amruth U <avcareer107@gmail.com>
2026-03-29 20:13:59 +0530

    DSA-12: tried solving monotonic stack problem

---
+++ DSA-12.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def removeKdigits(self, num, k):
+        l = list(num)
+        count = 0
+        s = ""
+        for i in range(len(num)-2):
+            print(count)
+            print(l)
+            print(s)
+            print("---------------")
+            s = s+l[i:i+2]
+            if count==k:
+                return s+l[-1]
+            if int(l[i])<int(l[i+1]):
+                s += l[i]
+                l.pop(i+1)
+                count +=1
+            #elif int(l[i])>int(l[i+1]):
+                #continue
+            else:
+                s+=l[i+1]
+                l.pop(i)
+                count+=1
+        return (s+l[-1])
+sol = Solution()
+print(sol.removeKdigits("10200",1))
