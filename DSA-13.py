07698c6d5e38161fb9a17b8df4bdebdd13ddcd5b
Amruth U <avcareer107@gmail.com>
2026-04-05 23:16:01 +0530

    DSA-13: solved exclusive time problem optimally

--- DSA-13.py
+++ DSA-13.py
@@ -22,3 +22,26 @@ class Solution(object):
     
 sol = Solution()
 print(sol.exclusiveTime(2,["0:start:0","1:start:2","1:end:5","0:end:6"]))
+
+class Solution(object):
+    def exclusiveTime(self, n, logs):
+        stack = []
+        res = [0] * n
+        prev_time = 0
+
+        for log in logs:
+            fid, typ, time = log.split(":")
+            fid = int(fid)
+            time = int(time)
+
+            if typ == "start":
+                if stack:
+                    res[stack[-1]] += time - prev_time
+                stack.append(fid)
+                prev_time = time
+
+            else:  # "end"
+                res[stack.pop()] += time - prev_time + 1
+                prev_time = time + 1
+
+        return res
