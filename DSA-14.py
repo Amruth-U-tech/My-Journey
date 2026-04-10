d400b3fd78a1716b9d3c86a4821c8dc63d15efa0
Amruth U <avcareer107@gmail.com>
2026-04-10 23:50:22 +0530

    DSA-14:revision of more easy questions

--- DSA-14.py
+++ DSA-14.py
@@ -17,4 +17,20 @@ class Solution(object):
             ans.append(nums[i])
             ans.append(nums[i+n])
         return ans
-        
+    def runningSum(self, nums):
+        sum1 = 0
+
+        for i in range(len(nums)):
+            sum1 += nums[i]
+            nums[i] = sum1
+        return nums
+    def pivotIndex(self, nums):
+        total = sum(nums)
+        left_sum = 0
+
+        for i in range(len(nums)):
+            right_sum = total-left_sum-nums[i]
+            if left_sum==right_sum:
+                return i
+            left_sum += nums[i]
+        return -1  
