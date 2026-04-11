c26d2281036bb8adf08605535dd0eb9416d2e2aa
Amruth U <avcareer107@gmail.com>
2026-04-11 23:46:42 +0530

    DSA-15:resolved old problem

--- DSA-15.py
+++ DSA-15.py
@@ -12,3 +12,18 @@ class Solution(object):
             key = tuple(encoder) #key must be immutable
             hashmap[key].append(word) #as default is list u can append directly
         return list(hashmap.values())              
+
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        ans = [1]*n
+
+        for i in range(1,n):
+            ans[i]*=nums[i-1]
+
+        right = n-2
+
+        for i in range(n):
+            ans[right]*=nums[right+1]
+            right-=1
+            
+        return ans
