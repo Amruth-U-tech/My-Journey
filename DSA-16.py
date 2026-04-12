3056ea7435f23b4f766f02a6a70b6e97125fde01
Amruth U <avcareer107@gmail.com>
2026-04-12 20:20:34 +0530

    DSA-16:learnt parity grouping

--- DSA-16.py
+++ DSA-16.py
@@ -7,4 +7,16 @@ class Solution(object):
                 nums[i],nums[index1]=nums[index1],nums[i]
                 index1+=1
         return nums
-        
+    
+    def minCostToMoveChips(self, position):
+        hashmap = {}
+        for i in position:
+            if i in hashmap:
+                continue
+            cost = 0
+            for j in position:
+                diff = abs(j-i)
+                if diff%2!=0:
+                    cost+=1
+            hashmap[i]=cost
+        return min(hashmap.values())
