eacfee422bbb9d969082b4c3d2099bf849cca274
Amruth U <avcareer107@gmail.com>
2026-04-14 15:20:22 +0530

    DSA-17:solved the anagram problem

--- DSA-17.py
+++ DSA-17.py
@@ -20,3 +20,28 @@ class Solution(object):
             if count>len(nums)//2:
                 return i
         return -1           
+
+#solved the anagram problem easy version
+
+    def isAnagram(self, s, t):
+        list1 = [[0]*26,[0]*26]
+
+        for letter in s:
+            index1 = ord(letter)-ord('a')
+            list1[0][index1]+=1
+        for letter in t:
+            index1 = ord(letter)-ord('a')
+            list1[1][index1]+=1
+        return list1[0]==list1[1]
+#solved the above problem by removing the redundant time complexity
+    def isAnagram(self, s, t):
+        count_s = [0]*26
+        count_t = [0]*26
+
+        for c in s:
+            count_s[ord(c)-ord('a')] += 1
+
+        for c in t:
+            count_t[ord(c)-ord('a')] += 1
+
+        return count_s == count_t
