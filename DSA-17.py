5d1399aec7e05e271fd541daf811494690c96c6b
Amruth U <avcareer107@gmail.com>
2026-04-14 15:08:23 +0530

    DSA-17:resolved the problem, better coding

--- DSA-17.py
+++ DSA-17.py
@@ -9,3 +9,14 @@ class Solution(object):
             if hashmap[i]>len(nums)//2:
                 return i
         return -1   
+#more proffesional code for same above problem
+    def majorityElement(self, nums):
+        hashmap = {}
+    
+        for i in nums:
+            hashmap[i] = hashmap.get(i,0)+1
+
+        for keys,count in hashmap.items():
+            if count>len(nums)//2:
+                return i
+        return -1           
