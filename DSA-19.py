b97e423326fdfbbb61f5374bc104e989c4b1dad3
Amruth U <avcareer107@gmail.com>
2026-04-16 17:11:42 +0530

    DSA-19: solved the previous problem optimmaly

---
+++ DSA-19.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        sum_len = 0
+        start = 0
+        end = 0
+        max_sum = float('-inf')
+        for i in range(len(nums)):
+            end+=1
+            sum_len = sum(nums[start:end])
+            if sum_len<0:
+                start = end
+            max_sum = max(max_sum,sum_len)
+        return max_sum
+#optimal code for the above problem
+    def maxSubArray(self, nums):
+        max_sum = float('-inf')
+        current_sum=0
+        for num in nums:
+            current_sum += num
+            max_sum = max(max_sum,current_sum)
+
+            if current_sum<0:
+                current_sum = 0
+        return max_sum 
