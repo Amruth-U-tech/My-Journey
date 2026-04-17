eae8f0b4ae0ec36d9ea7f389114cdf04dfaf5230
Amruth U <avcareer107@gmail.com>
2026-04-17 22:56:19 +0530

    DSA-20:used brute force sorting for sort colors

--- DSA-20.py
+++ DSA-20.py
@@ -7,3 +7,11 @@ class Solution(object):
                 if abs(nums[i]-nums[i+2])>=1 and nums[i]>nums[i+2]:
                     nums[i],nums[i+1]=nums[i+1],nums[i]
         return nums
+#used brute force sorting O(n^2)
+    def sortColors(self, nums):
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i]<nums[j]:
+                    nums[i],nums[j]=nums[j],nums[i]
+        return nums
+        
