9ddc627588fa90c5fa8a6a53c14c6bc40381ae19
Amruth U <avcareer107@gmail.com>
2026-04-20 19:40:14 +0530

    DSA-21:understood sliding window core concept

--- DSA-21.py
+++ DSA-21.py
@@ -16,5 +16,41 @@ class Solution(object):
             return 0
         return min_len
             
+#the issue with the above code is in case like [1,2,3,4,5] with target=11 it fails cause we reset the whole window to zero and start from scratch
+'''
+my logic:
+
+resets too early
+misses shorter valid windows
+⚔️ Conceptual Mistake
+
+You are doing:
+
+“expand → hit target → reset”
+
+Correct approach needs:
+
+“expand → shrink → find minimum”'''       
+
+#so tried to shrink the window instead of resetting it completly and found this way
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        min_len = float('inf')
+        surrent_sum = 0
+        current_len = 0
+        for end in range(len(nums)):
+            surrent_sum += nums[end]
+            current_len += 1
+            if surrent_sum>=target:
+                while surrent_sum>target:
+                    surrent_sum -= nums[end-current_len]
+                    current_len-=1
+                surrent_sum = 0
+                min_len = min(min_len,current_len)
+                current_len = 0
+                
+        if min_len==float('inf'):
+            return 0
+        return min_len
             
         
