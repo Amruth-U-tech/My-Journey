69a1579a50baf96ced5c9895434869af9b3eedaa
Amruth U <avcareer107@gmail.com>
2026-04-21 20:15:14 +0530

    DSA-22:resolved semi optimally (new python concept)

--- DSA-22.py
+++ DSA-22.py
@@ -15,3 +15,35 @@ class Solution(object):
     
 #while solving the above problem there is no issue in calculating the frequency by hashmap but getting top k frequent elements from hashmap 
 #directly becomes bit messy
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        hashmap = {}
+
+        for num in nums:
+            hashmap[num] = hashmap.get(num, 0) + 1
+
+        # sort by frequency
+        sorted_items = sorted(hashmap.items(), key=lambda x: x[1], reverse=True)
+
+        result = []
+        for i in range(k):
+            result.append(sorted_items[i][0])
+
+        return result
+# this is much better code clean and logical
+'''but this line was new to me and a unique concept in python
+    here lambda is anonymum function gerator and key is a anonymus function which takes x as an argument and return x[1]
+    
+    here x argument recives the tuple item i.e x=(key,value) fromt he hashmap and hence return x[1] that is values 
+        based on which the sorting is done
+        
+        ⚔️ Mental Model
+
+        Think:
+
+        for x in hashmap.items():
+            use x[1] to compare
+            
+            '''
+#there is much better way to solve this top k elemnt problem using heap tree i.e heapq (lib from pyhton) or bucket sort
